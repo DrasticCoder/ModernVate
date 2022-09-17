bc977f8bbbbb4d22875f46a2bae8a864d1671761
Drastic_Coder <deepbansodeofficial4205@gmail.com>
2022-09-17 20:31:30 +0530

    Create Info section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Nav from './components/Nav';
 import HeroSection from './components/HeroSection';
+import Info from './components/Info';
 
 function App() {
   return (
     <div>
       <Nav />
       <HeroSection />
+      <Info />
     </div>
   );
 }
