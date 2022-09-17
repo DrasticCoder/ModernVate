ee0fc7279817f734d3e05aaff9b17e5b15de2b73
Drastic_Coder <deepbansodeofficial4205@gmail.com>
2022-09-17 17:43:11 +0530

    Modify hero-section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Nav from './components/Nav';
+import HeroSection from './components/HeroSection';
 
 function App() {
   return (
     <div>
       <Nav />
+      <HeroSection />
     </div>
   );
 }
