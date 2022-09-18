05851871d4fa3691fe892573aaefa343b6713c79
Drastic_Coder <deepbansodeofficial4205@gmail.com>
2022-09-18 17:23:18 +0530

    Create Newsletter component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HeroSection from './components/HeroSection';
 import Info from './components/Info';
 import UserInfo from './components/UserInfo';
 import Features from './components/Features';
+import Newsletter from './components/Newsletter';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Info />
       <Features />
       <UserInfo />
+      <Newsletter />
     </div>
   );
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,9 +2,19 @@ import HeroImg from '../inventory/homepage.png';
 
 export default function HeroSection() {
     return (
-        <div className="bg-white">
+        <div className="md:relative overflow-hidden bg-white">
             <div className="mx-auto max-w-7xl">
-                <div className="z-2 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
+                <div className="md:relative z-2 pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
+                    <svg
+                        className="absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block"
+                        fill="currentColor"
+                        viewBox="0 0 100 100"
+                        preserveAspectRatio="none"
+                        aria-hidden="true"
+                    >
+                        <polygon points="50,0 100,0 50,100 0,100" />
+                    </svg>
+
                     <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                         <div className="sm:text-center lg:text-left">
                             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
